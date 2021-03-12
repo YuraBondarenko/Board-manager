97504cbaffe7d3026d4c5142b83d1bff22ca9c07
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-03-12 15:42:23 +0200

    updated models

--- src/main/java/com/example/boardmanager/model/Column.java
+++ src/main/java/com/example/boardmanager/model/Column.java
@@ -1,6 +1,7 @@
 package com.example.boardmanager.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,7 +16,7 @@ public class Column {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
     private String title;
 }
